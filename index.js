d57907c1e3f38953b08ea7cdf3adfa934c2378cd
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-28 17:32:41 +0600

    make admin api created

--- index.js
+++ index.js
@@ -65,6 +65,25 @@ async function run() {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
+
+    // make an user admin
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    });
     // upsert users
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
